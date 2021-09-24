d960e9aaf0c7f57838fd43ab28f43b15d3ac2bde
Brandon Anaya <89415505+brandon-anaya@users.noreply.github.com>
2021-09-23 22:09:52 -0700

    Create Restaurant_Bill.c

---
+++ Restaurant_Bill.c
@@ -0,0 +1,3 @@
+//Computes the tax and tip on a restaurant bill
+//Accepts the tax and tip both as percentages from the command line.
+//Display the meal cost, tax amount, tip amount, and total bill 
