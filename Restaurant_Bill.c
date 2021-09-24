b60074824df62b5cbb67c9106c967990269b6f60
Brandon Anaya <89415505+brandon-anaya@users.noreply.github.com>
2021-09-23 22:53:00 -0700

    Update Restaurant_Bill.c

--- Restaurant_Bill.c
+++ Restaurant_Bill.c
@@ -1,3 +1,21 @@
 //Computes the tax and tip on a restaurant bill
 //Accepts the tax and tip both as percentages from the command line.
 //Display the meal cost, tax amount, tip amount, and total bill 
+
+// Program computes the tax and tip amount.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double MealCost = 44.50,
+		   Tax		= MealCost * .0675,
+		   Tip 		= (MealCost + Tax) * .15,
+		   TBill	= MealCost + Tax + Tip;
+
+	cout << "Meal cost = $" << MealCost << endl;
+	cout << "Tax amount = $" << Tax << endl;
+	cout << "Tip amount = $" << Tip << endl;
+	cout << "Total bill = $" << TBill << endl;
+	return 0;
+}
